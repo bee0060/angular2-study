dd40a44149c093ddb2d0fa48e20e9eb83d8fb318
Steven Weng <Steven_Weng@epam.com>
2016-08-19 15:19:44 +0800

    change the root folder and add the tours of heroes project into here

---
+++ angular2-tour-of-heroes/README.md
@@ -0,0 +1,5 @@
+# angular2-study
+2nd Study angular 2 project
+
+
+The [link](https://angular.cn/docs/ts/latest/tutorial/toh-pt1.html) of angular 2 tour-of-heroes
---
+++ angular2-tour-of-heroes/app/app.routing.ts
@@ -0,0 +1,25 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+const appRoutes: Routes = [
+    {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+    },
+    {
+        path: 'heroes',
+        component: HeroesComponent
+    },{
+        path: 'dashboard',
+        component: DashboardComponent
+    },{
+        path: 'detail/:id',
+        component: HeroDetailComponent
+    }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
